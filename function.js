a4a51417142adacf02f176e91d2d03aba8abd7e5
Koder17 <DarkScript.Koder17@gmail.com>
2017-08-13 12:54:56 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-var apikey = "af1146a2df5582ec7a4b02c644eb2c1f"; // you can change your apikey here
+var apikey = "af1146a2df5582ec7a4b02c644eb2c1f";
 function getlink(photo_id){
     var xml = new XMLHttpRequest();
     xml.open("GET", "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + apikey + "&format=json&nojsoncallback=1&photo_id=" + photo_id, false);
@@ -8,3 +8,8 @@ function getlink(photo_id){
     return link;
 }
 console.log(getlink(document.URL.split("/")[5]))
+var a = document.createElement("a");
+a.download = getlink(document.URL.split("/")[5]).split("/")[4];
+a.href = getlink(document.URL.split("/")[5]);
+document.body.appendChild(a);
+a.click();
